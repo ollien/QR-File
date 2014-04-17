f6364b04347e6f866bf6c3b4564efa36181d9bc4
Nick Krichevsky <njk828@gmail.com>
2014-04-17 13:33:35 -0400

    Remove checkBaseURL function

--- qrl.py
+++ qrl.py
@@ -2,21 +2,6 @@ import qrcode
 import re
 import configReader
 from math import ceil
-def checkFullURL(string):
-	result=re.match('http://(www.)?\w.+/+\w*._',string) #This is regex to check for a url that begins with http://, possibly has www., and ends with /(something)_. Remove /+\w*._ to not check the ending.
-	if result:
-		return string
-	else:
-		result=re.match('http://.*',string)
-		if result:
-			raise ValueError("Not a valid URL.")
-		else:
-			result=re.match('http://*.','http://'+string)
-			if result:
-				return 'http://'+string
-			else:
-				raise ValueError("Not a valid URL.")
-	
 def checkBaseURL(string):
 	result=re.match('http://(www.)?\w.+/',string) #This is regex to check for a url that begins with http://, possibly has www., and ends with /
 	if result:
