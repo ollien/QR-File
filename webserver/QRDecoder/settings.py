49b0e639daf7fedb74a37d1bac574ed4a7838cec
Nick Krichevsky <njk828@gmail.com>
2014-04-18 01:06:54 -0400

    Add Checking for two parts

--- webserver/QRDecoder/settings.py
+++ webserver/QRDecoder/settings.py
@@ -56,18 +56,10 @@ WSGI_APPLICATION = 'QRDecoder.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default':{
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': 'QRDB',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306'
-
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
 }
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
--- webserver/QRDecoder/views.py
+++ webserver/QRDecoder/views.py
@@ -2,13 +2,59 @@ from django.http import HttpResponse
 import base64
 import zlib
 import magic
-
+import json
 def index(request):
 	return HttpResponse("Woo!")
 
 def makeData(request,file_id,part_id,base_64):
 	# return HttpResponse(base_64)
-	return HttpResponse(file_id+" "+part_id)
-	zipped=base64.b64decode(base_64)
-	unzipped=zlib.decompress(zipped)
-	return HttpResponse(unzipped,mimetype=magic.from_buffer(unzipped,mime=True))
+	#zipped=base64.b64decode(base_64)
+	#unzipped=zlib.decompress(zipped)
+	if (base_64[-1]!='_'):
+		cookies=request.COOKIES
+		parts=""
+		for cookieName in cookies.keys():
+			cookie=cookies[cookieName]
+			fail=False
+			parsedCookie=None
+			try:
+				parsedCookie=json.loads(cookie)
+			except:
+				fail=True
+				print 'failed'
+			print '--'
+			print parsedCookie
+			if not fail:
+				if file_id not in cookieName:
+					print 'file_id found...'
+					try:
+						cookies.remove(cookie)
+					except ValueError:
+						pass
+				else:
+					part=parsedCookie['part']
+					if part[-1]=='_':
+						parts+=part[:-1]
+					else:
+						parts+=part
+					print 'added '+part
+			parts+=base_64
+			print parts
+			zipped=base64.b64decode(parts)
+			unzipped=zlib.decompress(zipped)
+			return HttpResponse(unzipped,mimetype=magic.from_buffer(unzipped,mime=True))
+					
+		#return HttpResponse(unzipped,mimetype=magic.from_buffer(unzipped,mime=True))
+	else:
+		print type(json.dumps({
+	'fileId':file_id,
+	'partId':part_id,
+	'part':base_64
+}))
+		response=HttpResponse("Part added, add other parts")
+		response.set_cookie(file_id+'_'+part_id,json.dumps({
+			'fileId':file_id,
+			'partId':part_id,
+			'part':base_64
+		}),path='/decode')
+		return response
